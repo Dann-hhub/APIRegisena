32a48dbc8b043bd4390a17a64ecb6b57d5bffff1
Dann-hhub <espinosa081205@email.com>
2025-07-09 21:42:31 -0500

    Primer commit: subiendo mi API

---
+++ src/config.py
@@ -0,0 +1,9 @@
+class DevelopmentConfig():
+    DEBUG = True,
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_DB = 'regisena'
+
+config = {
+    'development' : DevelopmentConfig
+}
